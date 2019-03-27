271fcb939dbc2b562ccacc584d3d7afceb4bb464
Zachary Vanscoit <zachary816@gmail.com>
2019-03-27 17:16:33 -0500

    Added methods for checking match to a single input

--- MotifSearching/src/main/MotifLauncher.java
+++ MotifSearching/src/main/MotifLauncher.java
@@ -1,9 +1,12 @@
 package main;
 
+import tools.CP;
+
 public class MotifLauncher {
 
 	public static void main(String[] args) {
 		char[][] motifs = MatchMotifGenerator.getMotifs(8);
-		
+		CP.println("Key score for acgct: " + MotifMatcher.getScore(
+				new char[]{'a','c','g','c','t'}, new char[] {'a','c','g','c'}));
 	}
 }
--- MotifSearching/src/main/MotifMatcher.java
+++ MotifSearching/src/main/MotifMatcher.java
@@ -8,9 +8,12 @@ public class MotifMatcher {
 	 * @return -1 is returned if the lengths do not match.
 	 */
 	public static int getScore(char[] input, char[] key) {
+		// Initial check as to not waste time
 		if (input.length != key.length)
 			return -1;
+		
 		int score = 0;
+		// Check elements in the arrays
 		for (int i = 0; i < key.length; i++) {
 			if (key[i] == input[i])
 				score ++;
@@ -18,8 +21,18 @@ public class MotifMatcher {
 		return score;
 	}
 	
-	public static int[] getScores(char[] input, char[][] key) {
-		
-		return null;
+	/**<strong>getScores()</strong>
+	 * <p>Will take in a large array of keys and return a score for each of the keys.</p>
+	 * @param input char array to be checked against the key.
+	 * @param key char array of char arrays to check against the input.
+	 * @return -1 is returned for elements where the lengths of the arrays in key do not match.
+	 */
+	public static int[] getScores(char[] input, char[][] keys) {
+		int[] scores = new int[keys.length];
+		// Check elements in the arrays
+		for (int i = 0; i < keys.length; i++) {
+			scores[i] = getScore(input, keys[i]);
+		}
+		return scores;
 	}
 }
