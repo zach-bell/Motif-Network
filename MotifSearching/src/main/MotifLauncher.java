509de61982938a5003712e5bf79e017b5bb800ca
Zachary Vanscoit <zachary816@gmail.com>
2019-03-29 13:03:42 -0500

    Added test file to project

--- MotifSearching/src/main/MotifLauncher.java
+++ MotifSearching/src/main/MotifLauncher.java
@@ -17,9 +17,13 @@ public class MotifLauncher {
 		
 		LinkedList<MotifStruct[]> structList = new LinkedList<MotifStruct[]>();
 		char[][] motifKeys = MatchMotifGenerator.getMotifs(8);
-		char[][] input = mfh.getLineInFile("F:/User/Downloads/promoters_data_clean.txt");
-		for (int i = 0; i < input.length; i++) {
-			structList.add(MotifMatcher.scoreStreamInput(input[i], motifKeys, true));
+		char[][] input = mfh.getLineInFile("promoters_data_clean.txt");
+		try {
+			for (int i = 0; i < input.length; i++) {
+				structList.add(MotifMatcher.scoreStreamInput(input[i], motifKeys, true));
+			}
+		} catch (NullPointerException e) {
+			CP.println("Yea, couldn't run the rest of the program.");
 		}
 		CP.println("\nFinished scoring.\nTime taken: " + timer.stopTimeM() + "ms.");
 	}
